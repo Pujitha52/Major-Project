b2f1a1b374b56b334a5677f355f19f82e8759923
Pujitha52 <52529503+Pujitha52@users.noreply.github.com>
2021-07-14 19:41:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,13 @@
 import streamlit as st
 import joblib
-from sklearn import svm 
-
-
-filename = 'classifier.sav'
-classifier_linear=joblib.load(open(filename,'rb'))
-filename1 = 'vectorizer.sav'
-vectorizer=joblib.load(open(filename1,'rb'))
-
+model = joblib.load('Sentiment-Analysis')
 st.title('Sentiment Analysis using Machine Learning Algorithms')
-text=st.text_area('Enter text')
-
-st.set_option('deprecation.showfileUploaderEncoding', False)
-
-if st.button("Predict"):
-  review_vector = vectorizer.transform([text])
-  x=classifier_linear.predict(review_vector)
-  if (x[0]=='0'):
+ip = st.text_input('Enter your message')
+op = model.predict([ip])
+if st.button('Predict'):
+  if (op[0]=='0'):
     st.success('Text is  NEUTRAL')
-  if (x[0]=='1'):
+  if (op[0]=='1'):
     st.success('Text is POSITIVE')
-  if (x[0]=='-1'):
+  if (op[0]=='-1'):
     st.success(' Text is NEGATIVE')
